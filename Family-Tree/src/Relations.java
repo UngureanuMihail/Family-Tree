96d36395d0710421e44619533642309aff3d1559
Ungureanu Mihail <ungureanu.mmih@gmail.com>
2022-11-02 19:57:23 +0200

    Add TreeRelation module

--- Family-Tree/src/Relations.java
+++ Family-Tree/src/Relations.java
@@ -1,23 +1,28 @@
+import com.sun.source.tree.Tree;
+
 /*
 Класс для работы с отношениями между членами семьи
  */
 public class Relations {
     enum TreeRelationsType {
-        SPOUSE, PARENT, CHILD
+        spouse, parent, child
     }
 
     enum RelationsType {
-        FATHER, MOTHER, BROTHER, SISTER, SON, DAUGHTER, COUSIN, GRANDMOTHER, GRANDFATHER, GRANDSON, GRANDDAUGHTER, AUNT, UNCLE, HUSBAND, WIFE
+        father, mother, brother, sister, son, daughter, cousin,
+        grandmother, grandfather, grandson, granddaughter, aunt, uncle,
+        husband, wife
     }
 
     private Person person1;
     private Person person2;
+    private TreeRelationsType type;
 
-    Relations(Person person1, Person person2) {
+    Relations(TreeRelationsType type, Person person1, Person person2) {
         this.person1 = person1;
         this.person2 = person2;
+        this.type = type;
     }
-
 
     public Person getPerson1() {
         return person1;
@@ -33,5 +38,13 @@ public class Relations {
 
     public void setPerson2(Person person2) {
         this.person2 = person2;
+    }
+
+    public TreeRelationsType getType(){
+        return type;
+    }
+
+    public void setType(TreeRelationsType type) {
+        this.type = type;
     }
 }
