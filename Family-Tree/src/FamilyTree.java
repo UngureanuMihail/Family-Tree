3de5e7bc892c1a60601005f18b2c0969ce760665
Ungureanu Mihail <ungureanu.mmih@gmail.com>
2022-11-02 20:14:56 +0200

    Add Family Tree Class

--- Family-Tree/src/FamilyTree.java
+++ Family-Tree/src/FamilyTree.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class FamilyTree {
+import java.util.HashMap;
+import java.util.Map;
+
+public class FamilyTree {
+    private Person root;
+    private Map<String, Boolean> visited = new HashMap<>();
+
+    public Person getRoo() {
+        return root;
+    }
+
+    public void setRoot(Person root) {
+        this.root = root;
+    }
+
+    private Person.Gender fetchGender(Relations.RelationsType type) {
+        if (Relations.RelationsType.mother.equals(type) ||
+                Relations.RelationsType.daughter.equals(type) ||
+                Relations.RelationsType.wife.equals(type))
+            return Person.Gender.female;
+        else
+            return Person.Gender.male;
+    }
+
+    private Relations.TreeRelationsType fetchTreeRelationType(Relations.RelationsType type) {
+        if (Relations.RelationsType.mother.equals(type) || Relations.RelationsType.father.equals(type))
+            return Relations.TreeRelationsType.child;
+        else if (Relations.RelationsType.husband.equals(type) || Relations.RelationsType.wife.equals(type))
+            return Relations.TreeRelationsType.spouse;
+        else
+            return Relations.TreeRelationsType.parent;
+    }
 }
